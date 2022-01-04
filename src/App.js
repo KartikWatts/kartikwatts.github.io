28f83e7a5a0e100d968e5af42ddedd034bd43795
KartikWatts <arorakartik4@gmail.com>
2022-01-05 00:07:01 +0530

    loader animation done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+import Loader from "./Loader";
+import { useEffect, useState } from "react";
 
 function App() {
+	const [isLoading, setLoading] = useState(true);
+
+	function fakeRequest() {
+		return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+	}
+
+	useEffect(() => {
+		if (isLoading) {
+			console.log("useEffect called");
+			// fakeRequest().then(() => {
+			console.log("page loaded");
+			document.querySelector("#pre-loader").classList.add("loader-fade");
+			setLoading(!isLoading);
+			// });
+		}
+	}, [isLoading]);
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Kartik Watts
-				</a>
-			</header>
-		</div>
+		<>
+			<Loader />
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h3>
+						Under Development. <br />
+						Regards, Kartik Watts
+					</h3>
+				</header>
+			</div>
+		</>
 	);
 }
 
